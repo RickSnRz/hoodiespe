34142502913c8015a7160c840c3503b60aaedd46
RickSnRz <rickx213@gmail.com>
2024-05-15 15:04:45 -0500

    commit update

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -9,15 +9,15 @@ const HomeSectionCarosel = () => {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      items: 7,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      breakpoint: { max: 3000, min: 1028 },
+      items: 6,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      breakpoint: { max: 1024, min: 470 },
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
@@ -30,7 +30,7 @@ const HomeSectionCarosel = () => {
     .map((item) => <HomeSectionCard product={item} />);
 
   return (
-    <div className="relative p-5 border border-black">
+    <div className="relative p-5 border">
       <Carousel responsive={responsive}>{items}</Carousel>
     </div>
   );
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -9,6 +9,11 @@ const HomePage = () => {
 
       <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
         <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
+        <HomeSectionCarosel />
       </div>
     </div>
   )
